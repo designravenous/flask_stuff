35e50136d2ab9cff3ff7bec91348d222b857efda
Peter Holgersson <peterholgersson@Peters-iMac.local>
2019-01-27 17:39:05 +0100

    add login_required for index

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = "Mister H"
     title = "Welcome"
