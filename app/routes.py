b38b2e73c070112d04c22c2f241d7e4c8bca0b8e
Peter Holgersson <peterholgersson@Peters-iMac.local>
2019-02-02 10:26:44 +0100

    Add user.html and add link to profile in base.html

--- app/routes.py
+++ app/routes.py
@@ -62,10 +62,9 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = {
-        {'author':user, 'body':'Test post #1'},
-        {'author':user, 'body':'Test post #2'}
-    }
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
     return render_template('user.html', user=user, posts=posts)
 
-    
